5448b86d4f869db7ca9f3660f226501ca8f38fae
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-10-12 21:46:29 -0400

    Update _layout.tsx
    Updated layout.tsx file themes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  //Effect Handler Function
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
